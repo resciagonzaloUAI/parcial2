0c531e56ca03ee8a08f03492e478117bc440077e
resciagonzaloUAI <gonzalonicolas.rescia@alumnos.uai.edu.ar>
2022-07-04 21:29:59 -0300

    Se pushea el dashboard con su respectivo js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,10 +1,70 @@
 
-fetch('https://basic-server-one.vercel.app/users/')
+function validaCredenciales(credenciales) {
+  return fetch("https://basic-server-one.vercel.app/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: credenciales.email,
+      password: credenciales.pass,
+    }),
+  })
+    .then(function(respuesta) {
+      return respuesta.json()
+        .then(function (respuestaJSON) {
+          // Faltan datos
+          if (respuestaJSON.success === false) {
+            return false;
+          }
+          // Datos incorrectos
+          if (respuestaJSON.error === false) {
+            return true;
+          }
+          return false;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
 
-      .then(response => response.json())
-      .then(data => {
-        var respuesta = JSON.stringify(data)
-        document.querySelector('#debut').innerText = respuesta;
-        console.log(data)
-      });
-         
+function chequeaCredenciales (){
+  return localStorage.getItem("email") && localStorage.getItem("pass");
+}
+
+if (!chequeaCredenciales()) {
+    window.location.assign('/index.html')
+}
+
+const logoutUser = document.getElementById('logout');
+logoutUser.addEventListener("click", function(){
+  localStorage.clear();
+  window.location.assign("/index.html");
+});
+
+const tabla = (function (respuestaJson){
+  const tableContainer = document.getElementById('tableContainer');
+  for(let i of respuestaJson['data']){
+      tableContainer.innerHTML +=`
+      <tr class='tabla'>
+          <td class='tablat'>${i.id}</td>
+          <td class='tablat'>${i.name}</td>
+          <td class='tablat'>${i.username}</td>
+          <td class='tablat'>${i.email}</td>
+          <td class='tablat'>${i.address.street}</td>
+          <td class='tablat'>${i.address.suite}</td>
+          <td class='tablat'>${i.address.city}</td>
+          <td class='tablat'>${i.phone}</td>
+          <td class='tablat'>${i.company.name}</td>
+      </tr>`
+  }
+});
+fetch('https://basic-server-one.vercel.app/users')
+.then(function(respuesta){
+  return respuesta.json();
+})
+.then(function(respuestaJson){
+  tabla(respuestaJson);;
+});
