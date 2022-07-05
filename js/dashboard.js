4ed328d2ee2f713f6f318c85d7a5f41cd686784e
resciagonzaloUAI <gonzalonicolas.rescia@alumnos.uai.edu.ar>
2022-07-04 21:49:26 -0300

    Commit donde se le da estilo a la tabla

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,4 @@
-
-/* function validaCredenciales(credenciales) {
+function validaCredenciales(credenciales) {
   return fetch("https://basic-server-one.vercel.app/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -43,7 +42,7 @@ logoutUser.addEventListener("click", function(){
   localStorage.clear();
   window.location.assign("/index.html");
 });
-*/
+
 const tabla = (function (respuestaJson){
   const tableContainer = document.getElementById('tableContainer');
   for(let i of respuestaJson['data']){
